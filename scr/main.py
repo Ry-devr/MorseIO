ce694e35b8840185966a02ff9313f22072e9b4cb
Ry-devr <rtmz.developer@gmail.com>
2026-06-08 20:38:55 -0300

    Começando projeto

---
+++ scr/main.py
@@ -0,0 +1,15 @@
+import function
+
+while True:
+    print("Opçoes: ")
+    opcao = int(input("=> "))
+
+    if opcao == 1:
+        frase = input("Digite:\n=> ")
+        print(function.t_para_m(frase))
+
+    # elif opcao == 2:
+        # function.m_para_t()
+
+
+
